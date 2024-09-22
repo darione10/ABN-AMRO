68cf0732df88fbd297680086a63463e8744b4148
darione10 <57368001+darione10@users.noreply.github.com>
2024-09-22 10:38:53 +0200

    Revert "Delete setup.py"
    This reverts commit d428ab0598af1c69745d5bd8f08a44bf2877dfc2.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='app',
+        version='0.1.0',
+        author='Dario Gandini',
+        description='functions for prject',
+        long_description=open('README.md').read(),
+        python_requires='>=3.10')
